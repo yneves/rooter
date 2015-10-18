cd252fbeb78e471676635c880fbca72579895a18
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-10-18 13:50:58 -0200

    param in route

--- test/test-router.js
+++ test/test-router.js
@@ -9,15 +9,26 @@ var Router = require("../");
 
 describe("Router",function() {
 
-  it("it should match routes correctly",function() {
+  it("it should match simple routes",function() {
     var router = new Router();
     router.setRoute({
-      "/home": function() { return "home" },
-      "/login": function() { return "login" },
+      "/home": function() { return "home"; },
+      "/login": function() { return "login"; },
     });
     var route = router.findRoute("/login");
     assert.strictEqual(typeof route.handler,"function");
     assert.strictEqual(route.handler(),"login");
+  });
+  
+  it("it should match routes with params",function() {
+    var router = new Router();
+    router.setRoute({
+      "/item": function() {},
+      "/item/{id}": function() {}
+    });
+    var route = router.findRoute("/item/3");
+    assert.strictEqual(typeof route.handler,"function");
+    assert.strictEqual(route.params.id,"3");
   });
   
 });
